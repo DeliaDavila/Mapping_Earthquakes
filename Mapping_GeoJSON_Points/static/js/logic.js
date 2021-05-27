fd8a9849424c74714e3b4f07c058caf85285089c
DeliaDavila <alivad@gmail.com>
2021-05-27 10:52:06 -0500

    adding revised logic/index

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,82 +1,27 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport).addTo(map);
-
-
-// // Grabbing our GeoJSON data.
-// L.geoJson(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     pointToLayer: function(feature, latlng) {
-//       console.log(feature);
-//       return L.marker(latlng)
-//       .bindPopup("<h2>" + feature.properties.city + "</h2>");
-//     }
-
-//   }).addTo(map);
-
-
-// Grabbing our GeoJSON data...skill drill:  city, state, and the name of the airport. THERE IS NOT STATE IN THE DATA.
-// L.geoJson(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     pointToLayer: function(feature, latlng) {
-//       console.log(feature);
-//       return L.marker(latlng)
-//       .bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>  " + feature.properties.city + "</h3>");
-//      }
-
-//   }).addTo(map);
-
-
-//outdoor map.  popup:airport code and name of the airport.
-
-//onEachFeature
-  L.geoJson(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-    //   layer.bindPopup();
-      layer.bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>  " + feature.properties.faa + "</h3>");
-
-    }
-  }).addTo(map);
-
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 //base layer options
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    // id: 'mapbox/streets-v11',
-    //id: 'mapbox/outdoors-v11',
-   // id: 'mapbox/light-v10',
-    id: 'mapbox/dark-v10',
-    //id: 'mapbox/satellite-v9',
-    //id: 'mapbox/satellite-streets-v11',
+     id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
 
 // add base tile layer to the map
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+// //that probably should have had instruction to add GitHub name?
+let airportData = "https://raw.githubusercontent.com/DeliaDavila/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
